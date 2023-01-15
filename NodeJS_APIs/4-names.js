b5eb2f1d175d109b64b95eefbc373d8171d9c4c2
contactatfp <85082344+contactatfp@users.noreply.github.com>
2023-01-14 17:45:07 -0800

    globals, modules, utils

---
+++ NodeJS_APIs/4-names.js
@@ -0,0 +1,8 @@
+// LOCAL ONLY
+const secret = 'SUPER SECRET'
+
+// SHARED
+const john = 'john'
+const peter = 'peter'
+
+module.exports = {john, peter}
---
+++ NodeJS_APIs/7-mind-grenade.js
@@ -0,0 +1,11 @@
+// THIS MODULE WILL BE IMPORTED AND
+// THE FUNCTION WILL BE CALLED FROM APP.JS
+
+const num1 = 5
+const num2 = 10
+
+function addValues() {
+    console.log(`the sum is: ${num1 + num2}`)
+}
+
+addValues()
