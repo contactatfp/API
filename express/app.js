f84884f38f51442a456b28211616a39fa6ccc699
contactatfp <85082344+contactatfp@users.noreply.github.com>
2023-01-19 13:15:25 -0800

    updated app.js and package.json

--- express/app.js
+++ express/app.js
@@ -1,1 +1,24 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) =>{
+    res.writeHeader(200, {'Content-Type': 'text/html'})
+    const url = req.url
+    if(url === '/'){
+        res.write('<h1>Hello World</h1>')
+        res.end('Home Page')
+    }else if(url === '/about'){
+        res.writeHeader(200, {'Content-Type': 'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end('About Page')
+
+}else{
+    res.writeHeader(404, {'Content-Type': 'text/html'})
+    res.write('<h1>404</h1>')
+    }
+})
+
+server.listen(8000)
+
+
+
+
