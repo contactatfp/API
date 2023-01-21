bad7c284c737f0154b1ee9b884c65de1bcebac73
contactatfp <85082344+contactatfp@users.noreply.github.com>
2023-01-21 10:54:22 -0800

    setup API for products and singleProducts

--- express/app.js
+++ express/app.js
@@ -1,19 +1,33 @@
 const express = require('express')
-const path = require('path')
 const app = express()
+const { products } = require('./data')
 
-app.use(express.static(path.join(__dirname, './public')))
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './navbar-app/index.html'))
-//
-// })
-
-app.all('*', (req, res) => {
-    res.status(404).send('404 Not Found')
+app.get('/', (req, res) => {
+    // res.set('Content-Type', 'application/json')
+    // res.json(products)
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
 })
 
-app.listen('8000', () => {
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return { id, name, image }
+    })
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productID', (req, res) => {
+    const { productID } = req.params
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+    )
+    if (!singleProduct) {
+        return res.status(404).send('Product Does Not Exist')
+    }
+    return res.json(singleProduct)
+})
+
+
+app.listen(8000, () => {
     console.log('Server is running on port 8000')
 })
-
